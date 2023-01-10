3f1a6435dc277e9f82c50bfd2d3468dfed29aac6
sahilhirdhani <sahilhirdhani@gmail.com>
2023-01-10 21:51:32 +0530

    Day 1 - Intro

---
+++ Day 1 - Intro/Adding sript to webpage/external_file.js
@@ -0,0 +1,8 @@
+console.log("Hello from external file")
+function hello(){
+    document.write("Hello without button");
+};
+function hello1(){
+    document.getElementById("with_button").innerHTML = "Hello with buton";
+};
+
---
+++ Day 1 - Intro/intro_to_datatypes.js
@@ -0,0 +1,15 @@
+let firstName = 'Asabeneh'
+let lastName = 'Yetayeh'
+let age = 100, isMarried = true
+
+console.log("Data","Value","DataType")
+console.log("FirstName",firstName, typeof 'Asabeneh') 
+console.log("LastName", lastName, typeof lastName) 
+console.log("Age", age, typeof age)
+console.log("isMarried", isMarried, typeof isMarried)
+
+console.log("")
+const gravity = 9.81 // earth gravity  in m/s2
+const boilingPoint = 100 // water boiling point, temperature in °C
+const PI = 3.14 // geometrical constant
+console.log(gravity, boilingPoint, PI)
