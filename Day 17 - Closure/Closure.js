4e2ce8bfef2bfd04c296275fa0da2b2212d6cda1
sahilhirdhani <sahilhirdhani@gmail.com>
2023-01-26 21:02:09 +0530

    Day 17 - Closure

---
+++ Day 17 - Closure/Closure.js
@@ -0,0 +1,39 @@
+/* JavaScript allows writing function inside an outer function. We can write as many inner functions as we want. If inner function access the variables of outer function then it is called closure. */
+
+function outerFunction() {
+    let count = 0;
+    function innerFunction() {
+        count++
+        return count
+    }
+
+    return innerFunction
+}
+const innerFunc = outerFunction()
+
+console.log(innerFunc()) // 1
+console.log(innerFunc()) // 2
+console.log(innerFunc()) // 3
+
+// Another Example
+
+function outerFunction1() {
+    let count = 0;
+    function plusOne() {
+        count++
+        return count
+    }
+    function minusOne() {
+        count--
+        return count
+    }
+
+    return {
+        plusOne:plusOne(),
+        minusOne:minusOne()
+    }
+}
+const innerFuncs = outerFunction1()
+
+console.log(innerFuncs.plusOne) // 1
+console.log(innerFuncs.minusOne) // 0
